4dc1148f6e08c96c2239cadda9eceef450781488
Alexander Chelovekov <achelove@cisco.com>
2020-10-14 14:32:18 +0300

    Update

--- playbooks/no_scheduler_stage_2/1_per_vni_grabber.sh
+++ playbooks/no_scheduler_stage_2/1_per_vni_grabber.sh
@@ -1,8 +1,8 @@
 vni_list=(1001 1002 1003 1004)
 ansible_exec='/usr/bin/ansible-playbook'
-playbooks_dir='/home/cspc/ansible/playbooks/no_scheduler_stage_2'
-inventory='/home/olvs/ansible/inventories/INT/hosts.yml'
-extra_vars='/home/cspc/ansible/playbooks/no_scheduler_stage_2/extra_vars.yml'
+playbooks_dir='/home/cspc/ansible/pim-to-ir_drp/playbooks/no_scheduler_stage_2/pim-to-ir_drp'
+inventory='/home/olvs/ansible/pim-to-ir_drp/inventories/INT/hosts.yml'
+extra_vars='/home/cspc/ansible/pim-to-ir_drp/playbooks/no_scheduler_stage_2/extra_vars.yml'
 
 for t in ${vni_list[@]}; do
   echo ANSIBLE_HOST_KEY_CHECKING=False; $ansible_exec $playbooks_dir/1_generate_configs_per_vni.yml -i $inventory -e "@$extra_vars" -e "vni=$t"
