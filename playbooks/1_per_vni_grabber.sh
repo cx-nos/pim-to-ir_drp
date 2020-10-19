dd7c866f876f184a1e21dad4fde7669d8ed83565
Alexander Chelovekov <achelove@cisco.com>
2020-10-19 11:25:53 +0300

    Update

--- playbooks/1_per_vni_grabber.sh
+++ playbooks/1_per_vni_grabber.sh
@@ -4,7 +4,7 @@ env='STAN-1'
 ansible_exec='/usr/bin/ansible-playbook'
 playbooks_dir='/home/cspc/ansible/pim-to-ir_drp/playbooks/no_scheduler_stage_2'
 inventory='/home/cspc/ansible/pim-to-ir_drp/inventories/'${env}'/hosts.yml'
-extra_vars='/home/cspc/ansible/pim-to-ir_drp/playbooks/no_scheduler_stage_2/extra_vars.yml'
+extra_vars='/home/cspc/ansible/pim-to-ir_drp/playbooks/extra_vars.yml'
 
 for t in ${vni_list[@]}; do
   export ANSIBLE_HOST_KEY_CHECKING=False; $ansible_exec $playbooks_dir/1_generate_configs_per_vni.yml -i $inventory -e "@$extra_vars" -e "vni=$t"
